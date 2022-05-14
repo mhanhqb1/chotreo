8eec1911291912dd32f84a2a413e1985cd7805df
AnhMH <anh.mh@moziasoft.com>
2022-05-14 16:21:41 +0700

    Add authen

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
